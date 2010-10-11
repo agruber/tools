e18a7ef61f86d2c2cb92eb056e48d091baa224f5
Andreas Gruber <agruber@tbi.univie.ac.at>
2010-10-11 14:04:54 +0200

    small change in header comments

--- RNA/RealignStockholm.pl
+++ RNA/RealignStockholm.pl
@@ -9,9 +9,7 @@
 # This software is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.  You should have received a
-# copy of the GNU General Public License along with Paperpile.  If
-# not, see http://www.gnu.org/licenses.
+# General Public License for more details (http://www.gnu.org/licenses).
 
 use strict;
 use warnings;
